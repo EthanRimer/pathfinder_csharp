ce08d495703d5ef18a5f98ae63d79ae5dcec52ee
Ethan Rimer <47120616+EthanRimer@users.noreply.github.com>
2021-12-22 15:50:13 -0500

    Beginning C# implementation

--- Program.cs
+++ Program.cs
@@ -1,6 +1,59 @@
-﻿public class Pathfinder {
-    public static void Main(string[] args) {
+﻿using HtmlAgilityPack;
 
-    }
+class Page {
+    public string? title { get; set; }
+    public string? link { get; set; }
+    public string[]? children { get; set; }
 }
 
+class Hierarchy {
+    public static string[] links { get; set; }
+    public static Dictionary<string, Page> pages { get; set; }
+}
+
+public class Pathfinder {
+
+    private static HashSet<string> visitedLinks = new HashSet<string>();
+    private static HashSet<string> unvisitedLinks = new HashSet<string>();
+
+    public static string rootPage = @"https://www.peanuts.com";
+    static Page currentPage = new Page();
+
+    public static void Main(string[] args) {
+
+        Hierarchy h = new Hierarchy();
+
+        HtmlWeb web = new HtmlWeb();
+        var htmlDoc = web.Load(rootPage);
+
+        currentPage.link = rootPage;
+
+        string pageTitle = GetPageTitle(htmlDoc);
+        if(pageTitle != "") {
+            currentPage.title = pageTitle;
+        }
+
+        visitedLinks.Add(rootPage);
+    }
+
+    public static string GetPageTitle(HtmlDocument doc) {
+
+        string pageTitle = "";
+        try {
+
+            pageTitle = doc.DocumentNode.SelectSingleNode("//head/title").InnerText;
+        } catch(System.NullReferenceException) {
+
+            pageTitle = "";
+        }
+
+        return pageTitle;
+    }
+
+    /*
+    public static string[] FindLinks(HtmlDocument doc) {
+
+        return {""};
+    }
+    */
+}
